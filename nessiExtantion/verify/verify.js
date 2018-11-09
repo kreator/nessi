335af605cf93a7b0075f784758c116f250682a83
NatalyShv <nataly@kreator.im>
2018-11-09 10:51:50 +0200

    change

--- nessiExtantion/verify/verify.js
+++ nessiExtantion/verify/verify.js
@@ -8,8 +8,8 @@ chrome.storage.sync.get(['user_name', 'image_url'], function(result){
     document.getElementById("specText").innerHTML +=
      "<span>" + window.sender_persone_name + " wants to verify that you are " + window.certificate_type + "</span>";
 
-    document.getElementById("specImg").innerHTML +=
-      "<img src='../images/" + image_url + "' style='width:50%' ><span class='smallText'>"+ user_name +"</span>";
+  //  document.getElementById("specImg").innerHTML +=
+    //  "<img src='../images/" + image_url + "' style='width:50%' ><span class='smallText'>"+ user_name +"</span>";
 
 
 });
@@ -18,7 +18,8 @@ chrome.storage.sync.get(['user_name', 'image_url'], function(result){
 let verify = document.getElementById('verify');
 
 verify.onclick = function(element) {
-//call to the seconde persone chrome extention
+ sendSignedCertificate(window.certificate_type);
+ console.log("nnnnnnnn");
   window.close();
 };
 
